1f740f54309ab95515e5b76ace3c5c1c9fc40061
K <shusheng125@gmail.com>
2025-04-04 15:14:07 +0800

    add lambda_

--- my_pipeline/eks_stack.py
+++ my_pipeline/eks_stack.py
@@ -2,6 +2,8 @@ import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk import aws_eks as eks
 from aws_cdk import aws_ec2 as ec2
+import aws_cdk.aws_lambda as lambda_
+
 
 class EksStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -12,15 +14,15 @@ class EksStack(cdk.Stack):
             max_azs=2,
             nat_gateways=1)
 
-        # # Create the kubectl Layer
-        # kubectl_layer = lambda_.LayerVersion(self, "KubectlLayer",
-        #     code=lambda_.Code.from_asset('lambda/kubectl'),
-        #     compatible_runtimes=[lambda_.Runtime.PROVIDED_AL2]
-        # )
+        # Create the kubectl Layer
+        kubectl_layer = lambda_.LayerVersion(self, "KubectlLayer",
+            code=lambda_.Code.from_asset('lambda/kubectl'),
+             compatible_runtimes=[lambda_.Runtime.PROVIDED_AL2]
+        )
 
         # Create EKS Cluster
         cluster = eks.Cluster(self, "EksCluster",
-            version=eks.KubernetesVersion.V1_31,
+            version=eks.KubernetesVersion.V1_32,
             vpc=vpc,
             default_capacity=1,  # Number of nodes
             default_capacity_instance=ec2.InstanceType.of(
@@ -28,6 +30,5 @@ class EksStack(cdk.Stack):
                 ec2.InstanceSize.MEDIUM
             ),
             # kubectl_lambda_layer=kubectl_layer
-            #kubectl_layer=kubectl_layer
-            kubectl_layer=None  # Set to None if you don't need kubectl functionality
+            kubectl_layer=kubectl_layer
         )
